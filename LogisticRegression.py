c90d085c315d4e5eb49167376579d914402c55d7
Henry Dinh <Henry Dinh>
2016-03-07 11:24:18 -0600

    Set up weight dictionary where w0 is initially 0.0 and all other weights are initially 0.5.

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -13,6 +13,12 @@ import math
 training_set = dict()
 test_set = dict()
 
+# Vocabulary/tokens in the training set
+training_set_vocab = []
+
+# store weights as dictionary. w0 initiall 0.0, others initially 0.5. token : weight value
+weights = {'weight_zero': 0.0}
+
 # ham = 0 for not spam, spam = 1 for is spam
 classes = ["ham", "spam"]
 
@@ -34,6 +40,17 @@ def makeDataSet(storage_dict, directory, true_class):
                 storage_dict.update({dir_entry_path: Document(text, bagOfWords(text), true_class)})
 
 
+# Extracts the vocabulary of all the text in a data set
+def extractVocab(data_set):
+    all_text = ""
+    v = []
+    for x in data_set:
+        all_text += data_set[x].getText()
+    for y in bagOfWords(all_text):
+        v.append(y)
+    return v
+
+
 # counts frequency of each word in the text files and order of sequence doesn't matter
 def bagOfWords(text):
     bagsofwords = collections.Counter(re.findall(r'\w+', text))
@@ -42,7 +59,7 @@ def bagOfWords(text):
 
 # Learn weights
 def learnWeights():
-
+    pass
 
 
 # Document class to store email instances easier
@@ -83,6 +100,13 @@ def main(training_spam_dir, training_ham_dir, test_spam_dir, test_ham_dir):
     makeDataSet(training_set, training_ham_dir, classes[0])
     makeDataSet(test_set, test_spam_dir, classes[1])
     makeDataSet(test_set, test_ham_dir, classes[0])
+
+    # Extract training set vocabulary
+    training_set_vocab = extractVocab(training_set)
+
+    # Set all weights in training set vocabulary to be initially 0.5. w0 ('weight_zero') is initially 0.0
+    for i in training_set_vocab:
+        weights[i] = 0.5
 
     # Prints out the data set for testing purposes to make sure data is correctly read
     # for i in test_set:
